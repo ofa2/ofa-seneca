a061d126b4402eea7e07c3312cd5bd12e6360178
GongXian Cao <gongxiancao@hotmail.com>
2016-04-21 00:49:46 +0800

    use promise

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var seneca = require('seneca')();
 
-module.exports = function (done) {
-  var self = this;
-  self.seneca = seneca;
-  process.nextTick(done);
+module.exports = function () {
+  this.seneca = seneca;
 };
