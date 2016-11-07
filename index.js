725258c1d79d67308395d0288e5aed0bfdb25eea
GongXian Cao <gongxiancao@hotmail.com>
2016-11-07 23:20:14 +0800

    add shutdown command

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var Seneca = require('seneca');
 
 module.exports = function () {
-  this.seneca = Seneca(this.config.seneca.options);
+  var self = this;
+  var options = this.config.seneca.options;
+  options.system = options.system || {};
+  options.system.close_signals = {};
+
+  this.seneca = Seneca(options);
+  this.seneca.shutdown = function (done) {
+    self.seneca.act({role: 'seneca', cmd: 'shutdown'}, function (err) {
+      self.seneca.close(done);
+    });
+  };
 };
