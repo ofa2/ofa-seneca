6eeaed589711bea0ee9940db1d2dbd0bcbe5bde8
GongXian Cao <gongxiancao@hotmail.com>
2016-11-08 23:27:33 +0800

    move shutdown logic from ofa-seneca-route to ofa-seneca

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var Seneca = require('seneca');
+var Promise = require('bluebird');
 
-module.exports = function () {
+function lift (done) {
   var self = this;
   var options = this.config.seneca.options;
   options.system = options.system || {};
   options.system.close_signals = {};
-
   this.seneca = Seneca(options);
+  process.nextTick(done);
+};
+
+function lower (done) {
+  var self = this;
+  self.seneca.act({role: 'seneca', cmd: 'shutdown', 'default$': {}}, function (err) {
+    self.seneca.close(done);
+  });
+}
+
+module.exports = {
+  lift: Promise.promisify(lift),
+  lower: Promise.promisify(lower)
 };
